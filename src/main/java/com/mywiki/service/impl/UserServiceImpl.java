fec521a6d28993059633ffc0572def74be1a90b1
Kim Ngan <tkimngan040@gmail.com>
2026-08-17 23:00:49 +0700

    done login()

--- src/main/java/com/mywiki/service/impl/UserServiceImpl.java
+++ src/main/java/com/mywiki/service/impl/UserServiceImpl.java
@@ -1,12 +1,12 @@
 package com.mywiki.service.impl;
 
+import com.mywiki.model.dto.LoginRequest;
 import com.mywiki.model.dto.RegisterRequest;
 import com.mywiki.model.entity.User;
 import com.mywiki.repository.UserRepository;
 import com.mywiki.service.interfaces.UserService;
-import org.springframework.stereotype.Service;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -14,7 +14,10 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder
+    ) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
@@ -24,12 +27,14 @@ public class UserServiceImpl implements UserService {
 
         // Kiểm tra email đã tồn tại chưa
         if (userRepository.findByEmail(request.getEmail()).isPresent()) {
-            throw new RuntimeException("Email already exists!");
+            throw new RuntimeException("Email already exists");
         }
 
-        String passwordHash = passwordEncoder.encode(request.getPassword());
+        // Hash password
+        String passwordHash =
+                passwordEncoder.encode(request.getPassword());
 
-       // Tạo User mới
+        // Tạo User mới
         User user = new User(
                 request.getUsername(),
                 request.getEmail(),
@@ -38,5 +43,24 @@ public class UserServiceImpl implements UserService {
 
         // Lưu User vào database
         userRepository.save(user);
+    }
+
+    @Override
+    public boolean login(LoginRequest request) {
+
+        // Tìm User bằng email
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElse(null);
+
+        // Không tìm thấy User
+        if (user == null) {
+            return false;
+        }
+
+        // Kiểm tra password
+        return passwordEncoder.matches(
+                request.getPassword(),
+                user.getPasswordHash()
+        );
     }
 }
--- src/main/java/com/mywiki/service/interfaces/UserService.java
+++ src/main/java/com/mywiki/service/interfaces/UserService.java
@@ -1,9 +1,10 @@
 package com.mywiki.service.interfaces;
 
 import com.mywiki.model.dto.RegisterRequest;
+import com.mywiki.model.dto.LoginRequest;
 
 public interface UserService {
 
     void register(RegisterRequest request);
-
+    boolean login(LoginRequest request);
 }
